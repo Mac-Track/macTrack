61e9ce07f8237726dbb9b6677b849045d4a91c56
Nikki1686 <nikki.l.carthen@gmail.com>
2019-01-12 11:14:50 -0800

    homepage works

--- macTrack.sql
+++ macTrack.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS food_entry;
 DROP TABLE IF EXISTS exercise;
 
@@ -8,6 +8,7 @@ CREATE TABLE users (
   sex VARCHAR(255),
   age NUMERIC,
   height NUMERIC,
+  weight NUMERIC,
   activity_level NUMERIC,
   protein NUMERIC,
   fat NUMERIC,
@@ -37,3 +38,74 @@ CREATE TABLE exercise (
   calories NUMERIC
 );
 
+INSERT INTO users (
+  name,
+  sex,
+  age,
+  height,
+  weight,
+  activity_level,
+  protein,
+  fat,
+  carbs,
+  calories
+) VALUES (
+  'Towanda',
+  'female',
+  35,
+  170,
+  66,
+  1.4,
+  52,
+  43,
+  336,
+  1940
+);
+
+INSERT INTO food_entry (
+  fk_users,
+  date,
+  name,
+  image_url,
+  protein,
+  fat,
+  carbs,
+  calories,
+  serving_size,
+  serving_unit
+) VALUES (
+  1,
+  'Jan 12,2019',
+  'Banana',
+  'https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FtqnQdN4rDWU%2Fhqdefault.jpg',
+  1.3,
+  0.4,
+  27,
+  105,
+  1,
+  'banana'
+);
+
+INSERT INTO food_entry (
+  fk_users,
+  date,
+  name,
+  image_url,
+  protein,
+  fat,
+  carbs,
+  calories,
+  serving_size,
+  serving_unit
+) VALUES (
+  1,
+  'Jan 12,2019',
+  'Appletini',
+  'https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.bunkycooks.com%2Fwp-content%2Fuploads%2F2010%2F11%2FGreen-Appletini-close-up-2.jpg&f=1',
+  0,
+  0,
+  6.7,
+  149,
+  1,
+  'appletini'
+);
